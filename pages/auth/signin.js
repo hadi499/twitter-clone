6d7322ce65d5bb4f169e557307dd12742d24ebec
hadi499 <hadi.ygcl@gmail.com>
2023-04-13 12:48:54 +0700

    key

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -10,7 +10,7 @@ export default function signin({ providers }) {
       />
       <div className="">
         {Object.values(providers).map((provider) => (
-          <div className="flex flex-col items-center">
+          <div className="flex flex-col items-center" key={provider.id}>
             <img
               className="w-36 object-cover"
               src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"
