39df35026b8171c1747ff3c0865dea74ed83274a
hadi499 <hadi.ygcl@gmail.com>
2023-04-16 21:39:37 +0700

    get post

--- components/Post.jsx
+++ components/Post.jsx
@@ -7,6 +7,7 @@ import {
   TrashIcon,
 } from "@heroicons/react/outline";
 import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import Moment from "react-moment";
 
 export default function Post({ post }) {
   return (
@@ -14,7 +15,7 @@ export default function Post({ post }) {
       {/* user image */}
       <img
         className="h-11 w-11 rounded-full mr-4"
-        src={post.userImg}
+        src={post.data().userImg}
         alt="user-img"
       />
       {/* right side */}
@@ -24,9 +25,15 @@ export default function Post({ post }) {
         <div className="flex items-center justify-between">
           {/* post user info */}
           <div className="flex items-center space-x-1 whitespace-nowrap">
-            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post.name}</h4>
-            <span className="text-sm sm:text-[15px]">{post.username}</span>
-            <span className="text-sm sm:text-[15px] hover:underline">{post.timestamp}</span>
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
+              {post.data().name}
+            </h4>
+            <span className="text-sm sm:text-[15px]">
+              @{post.data().username} -{" "}
+            </span>
+            <span className="text-sm sm:text-[15px] hover:underline">
+              <Moment fromNow>{post.data().timestamp?.toDate()}</Moment>
+            </span>
           </div>
 
           {/* dot icon */}
@@ -36,12 +43,12 @@ export default function Post({ post }) {
         {/* post text */}
 
         <p className="text-gray-800 text-[15px sm:text-[16px] mb-2">
-          {post.text}
+          {post.data().text}
         </p>
 
         {/* post image */}
 
-        <img className="rounded-2xl mr-2" src={post.img} alt="" />
+        <img className="rounded-2xl mr-2" src={post.data().image} alt="" />
 
         {/* icons */}
 
